d0fccd51a49f49467da8527a584929bf2b1e1f5f
Dilshodbek <dilshodjonaqiyev@gmail.com>
2024-03-22 09:58:04 +0500

    first commit

---
+++ convex/auth.config.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  providers: [
+    {
+      domain: "https://logical-lioness-85.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
